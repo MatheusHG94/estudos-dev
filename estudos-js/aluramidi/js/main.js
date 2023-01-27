32bb648f21fb18302bbb36127a0eaf2b2e8e2e7b
Matheus <mat.hen.gra@gmail.com>
2023-01-27 16:22:08 -0300

    finalizando projeto midi

--- estudos-js/aluramidi/js/main.js
+++ estudos-js/aluramidi/js/main.js
@@ -1,54 +1,52 @@
-function tocaSom(idAudio) {
+function tocaSom(seletorAudio) {
+    let som = document.querySelector(seletorAudio);
 
-    let som = document.querySelector(idAudio);
-
-    som.play();
-
-    /*----------------------------------------------------------
-    var tecla = document.querySelector('.tecla_clap').classList[1];
-
-    let som = document.querySelector(`#som_${tecla}`);
-
-    som.play();
-    ---------------------------------------------------------*/
+    if (som && som.localName === 'audio') { //colocando apenas o nome do elemento 'som' o JS já entende que é para varificar se é diferente de nulo
+        som.play();
+    } else {
+        console.log('Elemento não encontrado ou inválido')
+    }
 }
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
-/*for (let i in listaTeclas) {
-    listaTeclas[i].onclick = function () { //função anônima
-        tocaSom('')
-    };
-}*/
+for (let i in listaTeclas) {
+    const tecla = listaTeclas[i];
+    const teclaPressionada = tecla.classList[1];
+    const idAudio = `#som_${teclaPressionada}`; //template string
 
-let i = 0;
-while (i <= listaTeclas.length) {
-    listaTeclas[i].onclick = function () { //função anônima
-        tocaSom('')
-    };
+    tecla.onclick = function () { //função anônima
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) { //evento, event ou e são nomes convencionalmente dados ao parâmetro que contém as especificidades do evento ao qual a função está associada
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
+/*let i = 0;
+while (i < listaTeclas.length) {
+    const tecla = listaTeclas[i];
+    const teclaPressionada = tecla.classList[1];
+    const idAudio = `#som_${teclaPressionada}`; //template string
+
+    tecla.onclick = function () { //função anônima
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) { //evento, event ou e são nomes convencionalmente dados ao parâmetro que contém as especificidades do evento ao qual a função está associada
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
     i++;
-}
-
-/* --------------------------------------------------------
-var tecla = document.querySelector('.tecla[]').classList[1];
-
-function tocaSomPom() {
-    let pom_som = document.querySelector('#som_tecla_pom');
-
-    pom_som.play();
-}
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-function tocaSomClap() {
-    let som_clap = document.querySelector('#som_tecla_clap');
-
-    som_clap.play()
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-var x = document.querySelector('.tecla_pom').classList[1];
-
-alert(x);
-*/
+}*/
