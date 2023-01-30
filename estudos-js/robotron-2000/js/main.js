a1d8a80cc86b8e2c12c408b9c5c6ffef36fef130
Matheus <mat.hen.gra@gmail.com>
2023-01-30 16:33:38 -0300

    continuando estudos

---
+++ estudos-js/robotron-2000/js/main.js
@@ -0,0 +1,63 @@
+/* -------------------- Primeiros passos -------------------- */
+
+function dizOla(nome) {
+    console.log(`Olá ${nome}! Bem-vindo ao Robotron 2000!`);
+}
+
+dizOla('Pedro');
+
+const robo = document.querySelector('.robo-js');
+
+//robo.onclick = function () { // função anônima
+//    console.log('Eu sou o Robotron 2000');
+//};
+
+//robo.addEventListener('click', function () { // chamando evento através do addEventListener('evento', função)
+//    console.log('Eu sou o Robotron 2000');
+//});
+
+//robo.addEventListener('click', () => { // arrow function
+//    console.log('Eu sou o Robotron 2000');
+//});
+
+robo.addEventListener('click', (evento) => { // o parâmetro 'evento' representa o event associado à função
+    console.log(evento);
+});
+
+/* -------------------- Modificando peças -------------------- */
+
+const controleAjuste = document.querySelectorAll('.controle-ajuste');
+
+function adicionarPeca(peca) {
+    const classePeca = `.contador-${peca}`;
+    const contador = document.querySelector(classePeca)
+    contador.value = Number(contador.value) + 1;
+}
+
+function subtrairPeca(peca) {
+    const classePeca = `.contador-${peca}`;
+    const contador = document.querySelector(classePeca)
+
+    if (Number(contador.value) > 0) {
+        contador.value = Number(contador.value) - 1;
+    } else {
+        alert('Não há mais peças do tipo para retirar.');
+    }
+}
+
+for (let i in controleAjuste) {
+    if (i % 2 == 0) {
+        const peca = controleAjuste[i].classList[1];
+        controleAjuste[i].addEventListener('click', function () {
+            subtrairPeca(peca);
+        });
+    } else {
+        const peca = controleAjuste[i].classList[1];
+        controleAjuste[i].addEventListener('click', function () {
+            adicionarPeca(peca);
+        });
+    }
+}
+
+
+
